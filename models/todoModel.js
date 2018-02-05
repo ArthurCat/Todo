c313640496bcf414366ffdf1040b443cef70319d
Yufei Zheng <arthurzheng9@gmail.com>
2018-02-05 01:49:12 -0500

    api update
    api updated

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
+
 var Schema = mongoose.Schema;
-var todoSchema = new  Schema({
+
+var todoSchema = new Schema({
     username: String,
     todo: String,
     isDone: Boolean,
